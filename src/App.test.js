836afdc551321d919e37a5e875932efef8a98fb6
Ales <aleswebgit@gmail.com>
2022-06-09 22:58:54 +0200

    test render app name, DDrop

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/start/i);
+  const linkElement = screen.getByText(/DDrop/i);
   expect(linkElement).toBeInTheDocument();
 });
